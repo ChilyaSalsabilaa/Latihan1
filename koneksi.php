5fb8bd5cf06b36510cb321ac7bd932943988ff41
Chilya Salsabilaa <156438201+ChilyaSalsabilaa@users.noreply.github.com>
2024-09-09 11:22:33 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','root','','pegawai');
+$conn=mysqli_connect('localhost','root','','pegawaii');
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
